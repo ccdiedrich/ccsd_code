36f94e8ca80e452f5752aa30fb656af8e7b44cb6
ccdiedrich <diedrichcody1212@gmail.com>
2017-03-28 16:04:03 -0700

    added A2
    added A2

---
+++ prg_102/week_01/assignments/A2/A2.m
@@ -0,0 +1,23 @@
+clear all;
+clc;
+
+cost=10
+price=12
+a = price — cost * 2 
+b = 15 + price — 3 * 2 
+c = (price + cost) * 3 
+d = 4 — 3 * 2 + cost 
+e = cost * ((price — 8) + 5) + 100
+
+% question 6a
+
+% Cody
+height_ft = input("please enter your height in ft\r\n")
+weight_lb = input("please enter your weight in lbs\r\n")
+height_cm = height_ft*12*2.54;
+weight_gr = weight_lb *453.59;
+gs = 1/3 * weight_gr + height_cm;
+printf("your gown size is = %.3f\r\n",gs);
+
+printf("your value of pi = %.10f\r\n",pi);
+
