d81acb04bf26a48125dd77f39a02fcfa24449e9c
ccdiedrich <diedrichcody1212@gmail.com>
2017-04-11 11:46:55 -0700

    update
    update

---
+++ prg_102/week_4/assignments/A1/A1.m
@@ -0,0 +1,16 @@
+ clear all;
+ clc;
+ 
+ 
+ string newSalesperson = "cody diedrich";
+      num newAmount
+      string usedSalesperson
+      num usedAmount
+      string bothAtEof = "N"
+      string HIGH_NAME = "ZZZZZ"
+      InputFile newSales
+      InputFile usedSales
+      OutputFile allsales 
+
+name = "cody diedrich";
+age = 26;
